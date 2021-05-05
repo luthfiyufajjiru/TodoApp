6e0ec086faccce8689345eef96caaf9d106990f4
Luthfi Yufajjiru <luthfiyufajjiru@gmail.com>
2021-05-05 13:40:17 +0700

    fixed

--- src/components/CardsContainer/index.js
+++ src/components/CardsContainer/index.js
@@ -2,10 +2,6 @@ import React from "react"
 import GroupCards from "../../components/GroupCards"
 
 class CardsContainer extends React.Component {
-    constructor(props) {
-        super(props)
-    }
-
     render() { return (
         <GroupCards title='Re-designed the zero-g doggie bags. No more spills!'  color="0, 140, 255"></GroupCards>
     )
